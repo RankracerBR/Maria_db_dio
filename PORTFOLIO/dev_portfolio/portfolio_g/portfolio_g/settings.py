9bf3b4c554914e8435944715c56a1a9dc334a53b
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-08-07 17:04:07 -0300

    project

--- PORTFOLIO/dev_portfolio/portfolio_g/portfolio_g/settings.py
+++ PORTFOLIO/dev_portfolio/portfolio_g/portfolio_g/settings.py
@@ -116,6 +116,16 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+AWS_ACCESS_KEY_ID = 'AKIAQAVOZSG5SLXCE7EO'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'staticportfolio1'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_REGION_NAME = 'sa-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATIC_URL = '/static/'
 
 
