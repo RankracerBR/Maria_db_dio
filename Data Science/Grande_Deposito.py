8f07f5037838db8d9cfeb88118b65e8d87aaaf2e
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2023-09-02 04:23:34 -0300

    Delete Data Science directory

--- Data Science/Grande_Deposito.py
+++
@@ -1,9 +0,0 @@
-valor = float(input())
-
-if valor > 0:
-    print(f'Deposito realizado com sucesso!')
-    print(f'Saldo atual: R$ {valor:.2f}')
-elif valor < 0:
-   print('Valor invalido! Digite um valor maior que zero.')
-else:
-  print('Encerrando o programa...')
